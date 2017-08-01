d22f41a8f03b72091286be8ac06802fbad182cfc
prashanth6708 <prashanth6708@gmail.com>
2017-08-01 16:08:46 -0500

    weight converter
    angula2 application

---
+++ src/assets/data2.js
@@ -0,0 +1,15 @@
+ function grams() {
+    var x=document.getElementById('txt').value;
+    document.getElementById('demo').innerHTML=x*453.592+"Grams";
+
+  }
+function kilograms() {
+  var x=document.getElementById('txt').value;
+  document.getElementById('demo').innerHTML=x*0.453592+"Kilograms";
+
+}
+function ounces() {
+  var x=document.getElementById('txt').value;
+  document.getElementById('demo').innerHTML=x*16+"Ounces";
+
+}
